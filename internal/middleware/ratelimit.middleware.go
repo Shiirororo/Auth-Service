e66a6da92dabf3bb86838452b865a8e1018a067f
Shiirororo <chacchankhongphainhan313@gmail.com>
2026-03-01 16:26:36 +0700

    Today I dont code :(

--- internal/middleware/ratelimit.middleware.go
+++ internal/middleware/ratelimit.middleware.go
@@ -1,1 +1,12 @@
 package middleware
+
+import "github.com/gin-gonic/gin"
+
+type RateLimitMiddleware struct {
+}
+
+func (r *RateLimitMiddleware) NewRateLimitMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
