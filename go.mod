adaaf35709a21eb914412e8286f0fa17081972fa
Shiirororo <chacchankhongphainhan313@gmail.com>
2026-03-05 23:26:11 +0700

    Update rate limiter using Library

--- go.mod
+++ go.mod
@@ -16,6 +16,8 @@ require (
 	gorm.io/gorm v1.31.1
 )
 
+require golang.org/x/time v0.14.0 // direct
+
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/BurntSushi/toml v1.6.0 // indirect
--- internal/middleware/ratelimit.middleware.go
+++ internal/middleware/ratelimit.middleware.go
@@ -1,20 +1,24 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
+	"golang.org/x/time/rate"
 )
 
-type RateLimitMiddleware struct {
-	rdb *redis.Client
-}
+var (
+	mu      sync.Mutex
+	clients = make(map[string]*Client)
+)
 
-type RateLimitMiddlewareInterface interface {
-	Allow(keyPrefix string, limit int, window time.Duration) gin.HandlerFunc
+type Client struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
+type RateLimitMiddleware struct {
 }
 
 // Key design:
@@ -22,48 +26,44 @@ type RateLimitMiddlewareInterface interface {
 // rl:<prefix>:user:{username}
 // rl:<prefix>:ip_user:{ip}:{username}
 
-func NewRateLimitMiddleware(rdb *redis.Client) RateLimitMiddlewareInterface {
-	return &RateLimitMiddleware{rdb: rdb}
-}
-
 func getClientIP(ctx *gin.Context) string {
 	ip := ctx.ClientIP()
-
+	if ip == "" {
+		ip = ctx.Request.RemoteAddr
+	}
 	return ip
 }
+func getRateLimiter(ip string) *rate.Limiter {
+	mu.Lock()
+	client, exist := clients[ip]
+	if !exist {
+		limiter := rate.NewLimiter(5, 10)
+		newClient := &Client{limiter, time.Now()}
+		clients[ip] = newClient
 
-func (r *RateLimitMiddleware) Allow(keyPrefix string, limit int, window time.Duration) gin.HandlerFunc {
+		return limiter
+	}
+	client.lastSeen = time.Now()
+	mu.Unlock()
+	return client.limiter
+}
+
+func CleanUpClients() {
+	for {
+
+	}
+}
+
+func RateLimiter() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx := c.Request.Context()
 		ip := getClientIP(c)
 
-		// Attempt to get a user identifier from the context, if set by an auth middleware
-		// You can customize the key "username" or "userID" based on your actual auth logic.
-		username := c.GetString("user-id")
+		limiter := getRateLimiter(ip)
 
-		var key string
-		if username != "" {
-			key = fmt.Sprintf("rl:%s:ip_user:%s:%s", keyPrefix, ip, username)
-		} else {
-			key = fmt.Sprintf("rl:%s:ip:%s", keyPrefix, ip)
+		if !limiter.Allow() {
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"error": "Too many request",
+			})
 		}
-
-		count, err := r.rdb.Incr(ctx, key).Result()
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Rate limiter error"})
-			return
-		}
-
-		if count == 1 {
-			// Set expiration window on the first request
-			r.rdb.Expire(ctx, key, window)
-		}
-
-		if count > int64(limit) {
-			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{"error": "Too Many Requests"})
-			return
-		}
-
-		c.Next()
 	}
 }
